db7b961b5ff91507fa0e873b74ec724e041ed817
Chaimae <bettah.chaimae@gmail.com>
2017-08-14 18:08:07 +0100

    add contact list

---
+++ src/ListContacts.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ListContacts extends Component {
+    render(){
+        const contacts = this.props.contact;
+        return (
+           <ol className="contact-list">
+               {contacts.map((contact) =>
+                   <li className="contact-list-item" key={contact.id}>
+                       <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})`}}/>
+                       <div className="contact-details">
+                           <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                       </div>
+                       <button className="contact-remove">Remove</button>
+                   </li>
+               )}
+           </ol>
+       )
+   }
+}
+
+export default ListContacts;
